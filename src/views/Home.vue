890718f08c84c4d5f1d475c311d0433be8a94671
Kate Howard <k.l.howard.1992@gmail.com>
2021-03-28 12:48:45 +0100

    Add vue-router

---
+++ src/views/Home.vue
@@ -0,0 +1,15 @@
+<template>
+  <router-link :to="`/game/${id}`">New game</router-link>
+</template>
+
+<script>
+import { v4 as uuid } from 'uuid'
+
+export default {
+  name: 'Home',
+  setup() {
+    const id = uuid();
+    return { id };
+  }
+}
+</script>
